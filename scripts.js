d92b15697b46fc1873bf7573e0a1cc667111fc5c
SendGpt <sendgptpvtltd@gmail.com>
2025-04-29 10:30:47 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Redirect to the Terms and Conditions page
+function showTerms() {
+    window.location.href = "terms.html";
+  }
+  
+  // Redirect to the Privacy Policy page
+  function showPrivacy() {
+    window.location.href = "privacy.html";
+  }
+  
